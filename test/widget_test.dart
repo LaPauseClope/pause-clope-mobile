8197a950f620092ea4992ca7a2d1bd1730f13a5d
sachabarbet <sacha.barbet@proton.me>
2025-04-24 09:36:26 +0200

    Remove test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:la_pause_clope/main.dart';
+
 
 void main() {
-  testWidgets('Default test widget'
-      '', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-  });
 }
