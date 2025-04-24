2d2f8b11263e091925b1c584b87c3cab23398078
MPFabio <128690691+MPFabio@users.noreply.github.com>
2025-04-24 11:23:25 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,32 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:la_pause_clope/pages/nickname_page.dart';
 
 void main() {
-  testWidgets('Default test widget'
-      '', (WidgetTester tester) async {
+  testWidgets('NicknamePage affiche le champ et le bouton', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: NicknamePage()));
+
+    expect(find.byType(TextField), findsOneWidget);
+    expect(find.text('Commençons le jeux !'), findsOneWidget);
+    expect(find.text('Entrez votre nom de joueur'), findsOneWidget);
+  });
+
+  testWidgets('Navigue vers ClickerPage si un nom est saisi', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: NicknamePage()));
+
+    // Entrer un pseudo
+    await tester.enterText(find.byType(TextField), 'TestUser');
+
+    // Lancer le bouton
+    await tester.tap(find.text('Commençons le jeux !'));
+    await tester.pump(); // lancement animations
+    await tester.pump(const Duration(milliseconds: 1300)); // laisser les animations se finir
+
+    // Vérifie qu’on a navigué vers une autre page
+    expect(find.byType(NicknamePage), findsNothing); // n’est plus sur NicknamePage
   });
 }
+
