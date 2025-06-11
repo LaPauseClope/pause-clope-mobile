39c561a5acb3f596d99915f81aab9387ecd0ef11
MPFabio <fabiomaratea@tutanota.com>
2025-06-11 12:36:28 +0200

    simulation erreur

--- scripts/release.sh
+++ scripts/release.sh
@@ -124,7 +124,7 @@ git add CHANGELOG.md
 
 echo "🏷 Création du commit et du tag $NEW_VERSION..."
 git commit -m "chore(release): $NEW_VERSION"
-# exit 42  # 💣 Simule une erreur fatale
+exit 42
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
 
 echo "Push vers origin/$RELEASE_BRANCH et le tag (forcé)..."
